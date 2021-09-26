23ac382386b4a68fdeb3b5a607ab3b8b66b56acc
samuel <samuelreyesiglesias@gmail.com>
2021-09-25 19:47:07 -0700

    WebApiConsoleAppClient Client of the WEB API.

---
+++ ConsoleAppClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClient
+{
+    class Program
+    {
+        public static Task<HttpResponseMessage> GetData()
+        {
+            HttpClient ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("http://localhost:56612/api/home/");
+            ApiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/plain"));
+            var data = ApiClient.GetAsync("index");
+            return data;
+        }
+        static void Main(string[] args)
+        {
+            HttpResponseMessage Response = GetData().Result;
+
+            if (Response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Susscessfull client request");
+                string StringDataResult = Response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"We get this data:{StringDataResult}");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong");
+            }
+        }
+    }
+}
