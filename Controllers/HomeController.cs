868edd4197d9b4e4191a51459c9f190f83cf9c22
samuel <samuelreyesiglesias@gmail.com>
2021-09-25 19:28:21 -0700

    web api net core and console app restfull client.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiNetCore.Controllers
+{
+    [Route("api/home")]
+    public class HomeController : Controller
+    {
+        [Produces("text/plain")]
+        [HttpGet("index")]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var content = "Hello world";
+                return Ok(content);
+            }catch(Exception Ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
